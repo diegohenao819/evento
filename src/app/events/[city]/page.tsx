7a4dc1110bc53d3bb94010ab9f422192513298c2
diegohenao819 <diegohenao819@gmail.com>
2024-07-16 22:15:33 -0500

    werwerw

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -22,30 +22,18 @@ const city = async ({ params }: cityProps) => {
   return (
     <main>
       {city === "all" ? (
-<<<<<<< HEAD
         <h1 className="text-3xl lg:text-6xl font-bold tracking-tight text-center">
           All events
         </h1>
       ) : (
         <h1 className="text-3xl lg:text-6xl font-bold tracking-tight text-center">
-=======
-        <h1 className="text-3xl lg:text-6xl font-bold tracking-tight mb-28 text-center">
-          All events.
-        </h1>
-      ) : (
-        <h1 className="text-3xl lg:text-6xl font-bold tracking-tight mb-28 text-center">
->>>>>>> 3809b7e3335b386372901583be0b2e25125a857d
           Events in: {city.toUpperCase().charAt(0) + city.slice(1)}
         </h1>
       )}
 
-<<<<<<< HEAD
       <div className="flex   flex-wrap mt-8 w-[1200px]  p-6 justify-center">
         <ListCards events={events} />
       </div>
-=======
-      <EventLists events={events} />
->>>>>>> 3809b7e3335b386372901583be0b2e25125a857d
     </main>
   );
 };
